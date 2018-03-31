53ca56a0e1c80bf96cd41e70f71b057411282c66
mikilauda95 <michele.simili95@gmail.com>
2018-03-31 17:25:12 +0200

    Some cleaning

--- python1/solution.py
+++ python1/solution.py
@@ -1,29 +1,35 @@
+# Import libraries
+
 import sys
 import string
 
+# Initialize list
 my_list=[[-1,"ciao"]]
+
+# Read the lines
 for line in sys.stdin:
-    for word in line.split( ):
-        if word.isdigit()==True:
-            # print(word)
+    for word in line.split():
+        # Check if it is a digit
+        if word.isdigit() == True:
             numbers=[x[0] for x in my_list]
             if word in numbers:
-                a=numbers.index(word)
-                my_list[a].append(line)
+                tmp=numbers.index(word)
+                my_list[tmp].append(line)
                 # print("already there")
             else:
                 my_list.append([word, line])
                 # print("I will add an element", word, line)
             break
 
-my_list.sort(key=lambda x: int(x[0]), reverse=True)
-# print(my_list)
+# Sort the list with respect to the first element
+my_list.sort(key = lambda x: int(x[0]), reverse=True)
+
+# Revert the list
 lines=[x[1:] for x in my_list[:-1]]
-# print(lines)
-a=1
+acc=1
 for i in lines:
     for j in i:
         print(j),
-    a+=1
-    if a>=int(sys.argv[1]):
+    acc+=1
+    if acc>=int(sys.argv[1]):
         exit()
--- python2/python2.py
+++ python2/python2.py
@@ -5,18 +5,21 @@ import sys
 import re
 import datetime
 import calendar
+
+# Define function for reading a line (until "\n")
 def recvline(sock):
 	line=""
 	while True:
 		a=sock.recv(1)
-		line+=a 
+		line+=a
 		if a=='\n':
-			break	
+			break
 	#print line
 	return line
 def findw(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
+#Open the connection
 sock = socket.socket()
 sock.connect(("127.0.0.1", 9999))
 recvline(sock)
@@ -27,14 +30,16 @@ sock.send(message)
 
 recvline(sock)
 
+#Hardcode the secret message
 message="e170f0ca3304f370139a297c0138afb8"
 message+='\n'
 
 sock.send(message)
-	
+
 recvline(sock)
 recvline(sock)
 
+# Start from this value and then do the binary search
 start=(2**20)/2 #first guess
 a=0
 num=start
@@ -48,9 +53,10 @@ while True:
 		num-=start/(2**a)
 	elif bigger in line.split( ):
 		num+=start/(2**a)
-	else:	
+	else:
 		break
 
+# Guess the the numbers drawn with asterisks
 numbers=[]
 num1=""
 num2=""
@@ -73,11 +79,10 @@ for line in num3.split('\n'):
 	a+=1
 	if a==8:
 		break
-	#print line
 	line+=' '*(7-len(line))
 	line+='\n'
 	modnum3+=line
-	
+
 five='#######\n#      \n#      \n ##### \n      #\n#     #\n ##### \n'
 onee='   #   \n  ##   \n # #   \n   #   \n   #   \n   #   \n ##### \n'
 nine=' ##### \n#     #\n#     #\n ######\n      #\n#     #\n ##### \n'
@@ -89,130 +94,12 @@ four='#      \n#    # \n#    # \n#######\n     # \n     # \n     # \n'
 twoo=' ##### \n#     #\n      #\n ##### \n#      \n#      \n#######\n'
 seve='#######\n#    # \n    #  \n   #   \n  #    \n  #    \n  #    \n'
 
-
-#
-#
-#	print num1 
-#	print "ciao"
-#	print num2 
-#	print "ciao"
-#	print modnum3 
-#	print "ciao"
-#	lnum1.append(num1)
-#	lnum2.append(num2)
-#	lnum3.append(modnum3)
-#
-#	print lnum1
-#	print lnum2
-#	print lnum3
-#	#for i in [num1, num2, num3]:
-#	#	print str(i),
-#while True:
-#	line=recvline(sock)
-#	if message in line.split( ):
-#		break
-#	sock.send(str(num)+'\n')
-#
-#
-#		
-#zero="""  ###  
-#         #   # 
-#        # #   #
-#        #  #  #
-#        #   # #
-#         #   # 
-#          ###  """
-#
-#onee="""   #   
-#          ##   
-#         # #   
-#           #   
-#           #   
-#           #   
-#         #####  """
-#
-#
-#twoo=""" #####
-#        #     #
-#              #
-#         #####
-#        # 
-#        #######"""
-#
-#
-#thre=""" #####
-#        #     #
-#              #
-#         #####
-#              #
-#        #     #
-#         ##### """
-#
-#
-#four="""#      
-#        #    # 
-#        #    # 
-#        #######
-#             # 
-#             # 
-#             # """
-#
-#
-#five="""#######
-#        #      
-#        #      
-#         ##### 
-#              #
-#        #     #
-#         ##### """
-#
-#
-#sixx=""" #####
-#        #     #
-#        #
-#        ######
-#        #     #
-#        #     #
-#         ##### """
-#
-#
-#seve="""#######
-#        #    # 
-#            #  
-#           #   
-#          #    
-#          #    
-#          #    """
-#
-#
-#eigh=""" ##### 
-#        #     #
-#        #     #
-#         ##### 
-#        #     #
-#        #     #
-#         ##### """
-#
-#
-#nine=""" ##### 
-#        #     #
-#        #     #
-#         ######
-#              #
-#        #     #
-#         ##### """
-#
 numbers=[zero,onee,twoo,thre,four,five,sixx,seve,eigh,nine]
 
 toguess=[num1,num2,modnum3]
-#print toguess
 guessed=""
 for num in toguess:
-#print num
-#print("ciao")
 	for orig in numbers:
-		#print(str(orig))
-		#print("ciao")
 		if num==str(orig):
 			#print("found")
 			guessed+=str(numbers.index(num))
@@ -220,8 +107,8 @@ for num in toguess:
 
 guessed+='\n'
 #print guessed
- 
- 
+
+
 recvline(sock)
 recvline(sock)
 sock.send(guessed)
@@ -240,42 +127,44 @@ sock.send(micros)
 recvline(sock)
 line=recvline(sock)
 lline=[word.strip(string.punctuation) for word in line.split()]
-#print lline
 date=lline[5:8]
-#print date
+
+# Build a dictionary for the months and weekdays.
+# Can be done using the formatting of datetime
+# Dictionary chosen for exercise
 d={}
-d["Jan"]="1"
-d["Feb"]="2"
-d["Mar"]="3"
-d["Apr"]="4"
-d["May"]="5"
-d["Jun"]="6"
-d["Jul"]="7"
-d["Aug"]="8"
-d["Sep"]="9"
-d["Oct"]="10"
-d["Nov"]="11"
-d["Dec"]="12"
+d["Jan"] = "1"
+d["Feb"] = "2"
+d["Mar"] = "3"
+d["Apr"] = "4"
+d["May"] = "5"
+d["Jun"] = "6"
+d["Jul"] = "7"
+d["Aug"] = "8"
+d["Sep"] = "9"
+d["Oct"] = "10"
+d["Nov"] = "11"
+d["Dec"] = "12"
 date[1]=d[date[1]]
 #print date[1]
-year=int("20"+date[2])
-date_obj=datetime.datetime(year, int(date[1]), int(date[0]))
+year = int("20"+date[2])
+date_obj = datetime.datetime(year, int(date[1]), int(date[0]))
 d_days={}
-d_days["0"]="Monday"
-d_days["1"]="Tuesday"
-d_days["2"]="Wednesday"
-d_days["3"]="Thursday"
-d_days["4"]="Friday"
-d_days["5"]="Saturday"
-d_days["6"]="Sunday"
+d_days["0"] = "Monday"
+d_days["1"] = "Tuesday"
+d_days["2"] = "Wednesday"
+d_days["3"] = "Thursday"
+d_days["4"] = "Friday"
+d_days["5"] = "Saturday"
+d_days["6"] = "Sunday"
 
 day=d_days[str(date_obj.weekday())]+'\n'
-#print day
+
 sock.send(day)
 recvline(sock)
 recvline(sock)
 secret=recvline(sock)
 secret=secret[:-1]
 lsecret=[secret]
-print lsecret
+print(lsecret)
 print(secret)
